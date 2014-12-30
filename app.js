a4e1be28c6b05b3d5b53d790cb5d9d3a96886861
jeremyko <jeremyko69@gmail.com>
2014-12-30 18:12:41 +0900

    postgres add

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '/bower_components'))); //20141229
+//app.use(express.static(path.join(__dirname, '/bower_components'))); //20141229
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/dust', db_api);
